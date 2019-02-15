5a55f2ec26c29811f8d6ec590ff0e1d094504633
Mark Lambert <marklam@spunow.co.uk>
2019-02-15 14:58:47 +0000

    Use events defined on an F# interface, from NSubstitute 3.1

---
+++ FSharpLibrary/UseInterface.fs
@@ -0,0 +1,11 @@
+﻿namespace FSharpLibrary
+
+open System.ComponentModel
+
+type IInterface =
+    [<CLIEvent>]
+    abstract Cancelable : IEvent<CancelEventHandler, CancelEventArgs>
+
+module UseInterface =
+    let hookupCancel (thing : IInterface) =
+        thing.Cancelable.Add (fun args -> args.Cancel <- true)
---
+++ Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using NSubstitute;
+using FSharpLibrary;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var thing = Substitute.For<IInterface>();
+            var cancelEventArgs = new CancelEventArgs();
+
+            UseInterface.hookupCancel(thing);
+
+            thing.Cancelable += Raise.Event<CancelEventHandler>(null, cancelEventArgs);
+
+            Debug.Assert(cancelEventArgs.Cancel);
+        }
+    }
+}
